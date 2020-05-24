7adc82b7dc54778c7ab8c17d2d0cf03d395cbf77
anathema6 <jzivovic96@gmail.com>
2020-05-24 11:43:28 +0200

    making tickets for basketball and tennis

--- easybet/frontend/src/app/make-your-own-ticket/make-your-own-ticket.component.ts
+++ easybet/frontend/src/app/make-your-own-ticket/make-your-own-ticket.component.ts
@@ -3,6 +3,10 @@ import { Observable } from 'rxjs';
 import { FootballMatch } from '../models/football-match.model';
 import { FootballService } from '../services/football.service';
 import { TicketService } from '../services/ticket.service';
+import { BasketballService } from '../services/basketball.service';
+import { BasketballMatch } from '../models/basketball-match.model';
+import { TennisService } from '../services/tennis.service';
+import { TennisMatch } from '../models/tennis-match.model';
 
 @Component({
   selector: 'app-make-your-own-ticket',
@@ -12,13 +16,39 @@ import { TicketService } from '../services/ticket.service';
 export class MakeYourOwnTicketComponent implements OnInit {
 
   public footballMatches: Observable<FootballMatch[]>;
+  public basketballMatches: Observable<BasketballMatch[]>;
+  public tennisMatches: Observable<TennisMatch[]>;
 
   constructor(private footballService: FootballService,
+              private basketballService: BasketballService,
+              private tennisService: TennisService,
               private ticketService: TicketService) {
     this.footballMatches = this.footballService.getFootballMatches();
+    this.basketballMatches = basketballService.getBasketballMatches();
+    this.tennisMatches = tennisService.getTennisMatches();
   }
 
-  public addToTicket(match: FootballMatch, event: Event){
+  public addToTicketFootball(match: FootballMatch, event: Event){
+    this.ticketService.addToTicket(
+      match._id,
+      match.time, 
+      match.homeTeam, 
+      match.guestTeam,
+      (<HTMLTableHeaderCellElement>event.target).id,
+      Number.parseFloat((<HTMLTableHeaderCellElement>event.target).textContent));
+  }
+
+  public addToTicketBasketball(match: BasketballMatch, event: Event){
+    this.ticketService.addToTicket(
+      match._id,
+      match.time, 
+      match.homeTeam, 
+      match.guestTeam,
+      (<HTMLTableHeaderCellElement>event.target).id,
+      Number.parseFloat((<HTMLTableHeaderCellElement>event.target).textContent));
+  }
+
+  public addToTicketTennis(match: TennisMatch, event: Event){
     this.ticketService.addToTicket(
       match._id,
       match.time, 
@@ -35,6 +65,8 @@ export class MakeYourOwnTicketComponent implements OnInit {
 
   public onCheckedChange(leagues: string[]){
     this.footballMatches = this.footballService.setFootballMatches(leagues); 
+    this.basketballMatches = this.basketballService.setBasketballMatches(leagues); 
+    this.tennisMatches = this.tennisService.setTennisMatches(leagues); 
   }
 
 
